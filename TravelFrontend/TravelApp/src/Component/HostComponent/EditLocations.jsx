fba384654f92c9fba00ced782753e475b939e4c9
ROHANCHOUKSEY <rohanchouksey02@gmail.com>
2025-07-14 18:59:18 +0530

    Update EditLocation

--- TravelFrontend/TravelApp/src/Component/HostComponent/EditLocations.jsx
+++ TravelFrontend/TravelApp/src/Component/HostComponent/EditLocations.jsx
@@ -8,6 +8,7 @@ import { useParams } from "react-router-dom";
 
 const EditLocations = () => {
   const { id } = useParams();
+  const [updating, setUpdating] = useState(false);
 
   const [editLocations, setEditLocations] = useState({
     editimage: [],
@@ -114,8 +115,8 @@ const EditLocations = () => {
   };
 
   const editButton = async () => {
-    let imageUrls = []; 
-
+    let imageUrls = [];
+    setUpdating(true);
     if (
       editLocations.editimage.length > 0 &&
       typeof editLocations.editimage[0] !== "string"
@@ -135,6 +136,8 @@ const EditLocations = () => {
       } catch (error) {
         console.log("Image upload Failed", error);
         return;
+      }finally{
+        setUpdating(false);
       }
     } else {
       imageUrls = editLocations.editimage;
@@ -167,6 +170,7 @@ const EditLocations = () => {
       timing: updateItem.timing,
       closing: updateItem.closing,
     });
+    setUpdating(true);
     window.location.href = "/host";
   };
 
@@ -289,7 +293,7 @@ const EditLocations = () => {
               className="w-full p-2 border rounded"
               value={editLocations.editVisitorTips}
               onChange={handleChange}
-            ></textarea> 
+            ></textarea>
           </div>
 
           <div>
@@ -321,8 +325,14 @@ const EditLocations = () => {
             className="w-full bg-blue-500 text-white p-2 rounded hover:bg-blue-600 cursor-pointer"
             onClick={editButton}
           >
-            Update
-          </button> 
+            {updating ? (<span className="flex items-center justify-center">
+              <svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+                <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+              </svg>
+              Updating...
+            </span>) : "Update"}
+          </button>
         </form>
       </div>
     </>
