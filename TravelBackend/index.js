9b02e8b12ed4e4a03f33aee0cf056ffe453ed380
ROHANCHOUKSEY <rohanchouksey02@gmail.com>
2025-07-19 21:21:24 +0530

    update

--- TravelBackend/index.js
+++ TravelBackend/index.js
@@ -42,7 +42,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: ['http://localhost:5173', 'https://travelapp-frontend-uaz9.onrender.com'],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
@@ -57,9 +57,9 @@ app.use(
     saveUninitialized: false,
     store: store,
     cookie: {
-      secure: false,
-      httpOnly: true,
-      sameSite: "lax",
+      secure: true,
+      // httpOnly: true,
+      sameSite: "none",
     },
   })
 );
