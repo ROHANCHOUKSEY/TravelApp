f80b6047cff5da1047f6c5e54ec75f1fc1f004f8
ROHANCHOUKSEY <rohanchouksey02@gmail.com>
2025-07-19 16:51:47 +0530

    update

--- TravelBackend/index.js
+++ TravelBackend/index.js
@@ -42,7 +42,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: [process.env.FRONTEND_URL || "https://travelapp-frontend-4b88.onrender.com"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
